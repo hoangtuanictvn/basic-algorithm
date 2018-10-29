a36cd9c4bf055b75783021d4d051c9cd610d51b1
Hoang Tuan <hoangtuanictvn@gmail.com>
2018-10-29 23:00:22 +0700

    Moving from C to Golang

--- include/log.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LOG_H_
-#define _LOG_H_
-#include<stdio.h>
-#include <errno.h>
-#define clean_errno() (errno == 0 ? "None" : strerror(errno))
-#define loge(logs, ...) fprintf(stderr, "[ERROR - line:%d] in FILE[%s] at FUNC[%s]:" logs "\n", __LINE__, __FILE__, __func__, clean_errno(), ##__VA_ARGS__)
-#define logw(logs, ...) fprintf(stderr, "[WARN - line:%d] in FILE[%s] at FUNC[%s]:" logs "\n",__LINE__, __FILE__, __func__, clean_errno(), ##__VA_ARGS__)
-#define logi(logs, ...) fprintf(stderr, "[INFO - line:%d] in FILE[%s] at FUNC[%s]:" logs "\n", __LINE__, __FILE__, __func__, clean_errno(), ##__VA_ARGS__)
-#endif
--- tests/main.c
+++
@@ -1,9 +0,0 @@
-#include <eval.h>
-#include<stdio.h>
-int main(int argc, char const *argv[])
-{
-    Object obj;
-    obj = new_i(10);
-    printf("Integer:%d\n",get_i(obj));
-    return 0;
-}
